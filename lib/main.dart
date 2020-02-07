f436247fe1dc7d1cc019d4b21b7dab749557a66d
Paula Penedo <penedo@work.co>
2020-02-07 17:25:33 -0300

    refactor(transaction): break into multiple components and add state

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:uuid/uuid.dart';
 
-import './constants/transaction.dart';
-
-import './utils/Intl.dart';
+import './widgets/userTransactions.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,37 +15,15 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  static final uuid = new Uuid();
-
-  final List<Transaction> transactions = [
-    Transaction(
-        id: uuid.v1(),
-        title: 'Shoes',
-        amount: 59.99,
-        createdAt: DateTime.now(),
-        updatedAt: DateTime.now()),
-    Transaction(
-        id: uuid.v1(),
-        title: 'Groceries',
-        amount: 102.99,
-        createdAt: DateTime.now(),
-        updatedAt: DateTime.now()),
-    Transaction(
-        id: uuid.v1(),
-        title: 'Light Bill',
-        amount: 1.99,
-        createdAt: DateTime.now(),
-        updatedAt: DateTime.now()),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               width: double.infinity,
@@ -57,55 +32,10 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  TextField(decoration: InputDecoration(labelText: 'Title')),
-                  TextField(decoration: InputDecoration(labelText: 'Amount')),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text('Add Transaction'),
-                    textColor: Colors.black,
-                  )
-                ],
-              ),
-            ),
-            Column(
-              children: transactions.map((transaction) {
-                return Card(
-                    child: Row(
-                  children: <Widget>[
-                    Container(
-                      width: 120,
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      padding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                      child: Text(
-                        'R\$ ${transaction.amount}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(transaction.title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold)),
-                        Text(
-                          formatDate(transaction.createdAt),
-                          style: TextStyle(fontSize: 14),
-                        ),
-                      ],
-                    )
-                  ],
-                ));
-              }).toList(),
-            )
+            UserTransactions(),
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      ),
+    ); // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
---
+++ lib/widgets/transactionList.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../models/transaction.dart';
+import '../utils/Intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 120,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  child: Text(
+                    'R\$ ${transactions[index].amount}',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(transactions[index].title,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
+                    Text(
+                      formatDate(transactions[index].createdAt),
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
